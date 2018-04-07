3f77a5ce71236546b3500dc9c74196b2b59fbe3b
coderman9 <coderman9@gmail.com>
2018-04-06 21:14:32 -0600

    changed > to <

--- read.py
+++ read.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from PIL import Image
 import math
 import sys
-import codecs
 def mean(vals):
 	m=0
 	for i in vals:
@@ -19,11 +18,11 @@ for i in range(height):
 		cpix=pixel[i*width+j]
 		#magic numbers found online
 		brightness[i][j]=(0.299*cpix[0]**2+0.587*cpix[1]**2+0.114*cpix[2]**2)**0.5
-with codecs.open("blockImage.txt", "w", "utf-8") as ti:
+with open("blockImage.txt", "w") as ti:
 	for i in range(0, height, 2):
 		for j in range(0, width, 2):
-			if mean((brightness[i][j], brightness[i+1][j], brightness[i][j+1], brightness[i+1][j+1], brightness[i+2][j]))>128:
-				ti.write(u'\u25A0')
+			if mean((brightness[i][j], brightness[i+1][j], brightness[i][j+1], brightness[i+1][j+1], brightness[i+2][j]))<128:
+				ti.write('.')
 				#ti.write(".")
 			else:
 				ti.write(' ')
