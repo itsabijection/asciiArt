0b5ac7e519c71429ec34e5ba221f051e53e89971
coderman9 <coderman9@gmail.com>
2018-04-06 21:30:32 -0600

    added support for reversing colors

--- read.py
+++ read.py
@@ -5,8 +5,17 @@ import sys
 def mean(vals):
 	m=0
 	for i in vals:
-		m+=i
+		if i:
+			m+=i
 	return(m/len(vals))
+
+dark="."
+light=" "
+#reverse colors
+if len(sys.argv)>2 and int(sys.argv[2])==1:
+	temp=light
+	light=dark
+	dark=temp
 
 im=Image.open(sys.argv[1])
 width, height=im.size
@@ -19,11 +28,10 @@ for i in range(height):
 		#magic numbers found online
 		brightness[i][j]=(0.299*cpix[0]**2+0.587*cpix[1]**2+0.114*cpix[2]**2)**0.5
 with open("blockImage.txt", "w") as ti:
-	for i in range(0, height, 2):
-		for j in range(0, width, 2):
+	for i in range(0, height-2, 2):
+		for j in range(0, width-2, 2):
 			if mean((brightness[i][j], brightness[i+1][j], brightness[i][j+1], brightness[i+1][j+1], brightness[i+2][j]))<128:
-				ti.write('.')
-				#ti.write(".")
+				ti.write(dark)
 			else:
-				ti.write(' ')
+				ti.write(light)
 		ti.write("\n")
